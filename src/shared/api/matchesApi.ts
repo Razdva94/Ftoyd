afbeb2ba7b9fe673c4a1d54a5902c8cd58ae973b
Antonov Danila <115868609+Razdva94@users.noreply.github.com>
2025-03-20 11:40:23 +0300

    Update matchesApi.ts

--- src/shared/api/matchesApi.ts
+++ src/shared/api/matchesApi.ts
@@ -1,7 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { IData } from '@/interfaces';
 
-// Создаём API Slice
 export const matchesApi = createApi({
 	reducerPath: 'matchesApi',
 	baseQuery: fetchBaseQuery({ baseUrl: `${import.meta.env.VITE_URL}` }),
