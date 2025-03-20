83ea1f84a14d33ebd9a38ac64179d9ad85e6f3db
Antonov Danila <115868609+Razdva94@users.noreply.github.com>
2025-03-20 11:40:59 +0300

    Update Header.tsx

--- src/entities/header/ui/Header.tsx
+++ src/entities/header/ui/Header.tsx
@@ -1,26 +1,3 @@
-// import logo from '@/assets/images/match-tracker.svg';
-// import HeaderButton from './HeaderButton';
-// import HeaderErrorMessage from './HeaderErrorMessage';
-// import { RootState } from '@/store';
-// import { useSelector } from 'react-redux';
-
-// const Header = () => {
-// 	const isErrorVisible: boolean = useSelector(
-// 		(state: RootState) => state.errorToggleHandler.isErrorVisible,
-// 	);
-// 	return (
-// 		<header className="w-full flex justify-between">
-// 			<img src={logo} alt="logo" />;
-// 			<div className="flex gap-5">
-// 				{isErrorVisible && <HeaderErrorMessage />}
-// 				<HeaderButton />
-// 			</div>
-// 		</header>
-// 	);
-// };
-
-// export default Header;
-
 import logo from '@/assets/images/match-tracker.svg';
 import HeaderButton from './HeaderButton';
 import HeaderErrorMessage from './HeaderErrorMessage';
